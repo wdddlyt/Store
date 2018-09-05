3072e3b665f1b07e401d648070db3265c85cc0bf
wdddlyt <346032117@qq.com>
2018-09-05 21:31:43 +0800

    项目创建

---
+++ MeiDo/meiduo/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MeiduoConfig(AppConfig):
+    name = 'meiduo'
---
+++ MeiDo/meiduo/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import url
+
+app_name='meiduo'
+
+
+urlpatterns = [
+
+
+]
